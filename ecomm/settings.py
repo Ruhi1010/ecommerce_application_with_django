46c6f17259d4f7207c2d5fee833bee5233c64867
Ruhi1010 <rashedial52@gmail.com>
2026-09-08 04:07:57 +0600

    .

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -147,8 +147,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'rashedial52@gmail.com'
-# EMAIL_HOST_PASSWORD = 'swzi mzbg bdfc hmqt'
+
 
 
 
