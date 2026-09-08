3196668200748c68336e5cbcb4b948a707bd5d1e
Ruhi1010 <rashedial52@gmail.com>
2026-09-09 00:09:32 +0600

    .

--- products/models.py
+++ products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 from base.models import BaseModel
+from django.utils.text import slugify
 
 
 class Category(BaseModel):
@@ -9,6 +10,24 @@ class Category(BaseModel):
     slug = models.SlugField(unique=True, null=True, blank=True)
     category_image = models.ImageField(upload_to = "categories")
 
+    
+    
+    def save(self, *args, **kwargs):
+            self.slug = slugify(self.category_name)
+            super(Category, self).save(*args, **kwargs)
+            
+            
+            
+            
+    def __str__(self):
+            return self.category_name
+    
+    
+    
+    
+    
+    
+    
 
 
 class Product(BaseModel):
@@ -19,6 +38,16 @@ class Product(BaseModel):
     product_description = models.TextField()
     
     
+    def save(self, *args, **kwargs):
+                self.slug = slugify(self.product_name)
+                super(Product, self).save(*args, **kwargs)
+                
+                
+                
+                
+    def __str__(self):
+                return self.product_name
+    
     
     
     
@@ -28,5 +57,5 @@ class Product(BaseModel):
 class ProductImage(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_images")
     image = models.ImageField(upload_to="product")
-    product_description = models.TextField()
+    # product_description = models.TextField()
     
